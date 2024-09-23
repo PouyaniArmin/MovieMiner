f64422524f8cd10b3cfcdd44b3600d7b093fcc32
Pouyani Armin <pouyaniarmin@gmail.com>
2024-09-23 08:53:43 +0330

    test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace Tests;
+
+use PHPUnit\Event\Code\Test;
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase{
+    public function testTrueIsTrue(){
+        $this->assertTrue(true);
+    }
+}
