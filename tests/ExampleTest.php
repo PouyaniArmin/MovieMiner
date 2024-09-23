c801de487121fcfa93709488d06969574a66286f
Pouyani Armin <pouyaniarmin@gmail.com>
2024-09-23 11:13:45 +0330

    test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,7 +6,7 @@ use PHPUnit\Event\Code\Test;
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase{
-    public function testTrueIsTrue(){
+    public function testTrueIsTrue():void{
         $this->assertTrue(true);
     }
 }
