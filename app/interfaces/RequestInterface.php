7abc7c57a1084044d93bae6c344d35a7b795e848
Pouyani Armin <pouyaniarmin@gmail.com>
2024-09-26 13:18:39 +0330

    request interface

---
+++ app/interfaces/RequestInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Interfaces;
+
+interface RequestInterface
+{
+    public function path(): string;
+    public function method(): string;
+    public function isGet(): bool;
+    public function isPost(): bool;
+    public function isPut(): bool;
+    public function isPatch(): bool;
+    public function isDelete(): bool;
+    public function isHead(): bool;
+    public function isOptions(): bool;
+    public function isMethod(string $method): bool;
+    public function setInput($input): void;
+    public function body(): array;
+}
