42d17f1167b8e59c6637d45a8241105974963864
Pouyani Armin <pouyaniarmin@gmail.com>
2024-09-30 11:14:50 +0330

    Error handler

---
+++ app/Utility/ErrorHandler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Utility;
+
+use Exception;
+
+class ErrorHandler
+{
+    public static function errorMessage(string $message, int $code = 0): Exception
+    {
+      return throw new Exception($message, $code);
+    }
+}
